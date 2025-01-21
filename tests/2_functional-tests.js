1e67dd6a8bacdda9209a37a1c6bc2614fd529ca6
C00P3R14 <ngubane.linda9@gmail.com>
2025-01-21 16:15:10 +0200

    all functional tests done

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -17,7 +17,47 @@ suite('Functional Tests', function() {
                 assert.equal(res.text,'{"initNum":10,"initUnit":"l","returnNum":2.6417217685798895,"returnUnit":"gal","string":"10 liters converts to 2.6417217685798895 gallons"}')
                 done()
             })
-        })
+        });
+        test("Test GET /api/convert with invalid input",(done) => {
+            chai.request(server)
+            .keepOpen()
+            .get("/api/convert?input=32g")
+            .end((req, res) => {
+                assert.equal(res.status,200);
+                assert.equal(res.text,"invalid unit")
+                done()
+            })
+        });
+        test("Test GET /api/convert with invalid number input",(done) => {
+            chai.request(server)
+            .keepOpen()
+            .get("/api/convert?input=3/7.2/4kg")
+            .end((req, res) => {
+                assert.equal(res.status,200);
+                assert.equal(res.text,"invalid number")
+                done()
+            })
+        });
+        test("Test GET /api/convert with invalid number and unit input",(done) => {
+            chai.request(server)
+            .keepOpen()
+            .get("/api/convert?input=3/7.2/4g")
+            .end((req, res) => {
+                assert.equal(res.status,200);
+                assert.equal(res.text,"invalid number and unit")
+                done()
+            })
+        });
+        test("Test GET /api/convert with no number input",(done) => {
+            chai.request(server)
+            .keepOpen()
+            .get("/api/convert?input=kg")
+            .end((req, res) => {
+                assert.equal(res.status,200);
+                assert.equal(res.text,'{"initNum":1,"initUnit":"kg","returnNum":2.2046244201837775,"returnUnit":"lbs","string":"1 kilograms converts to 2.2046244201837775 pounds"}')
+                done()
+            })
+        });
     })
 
 });
