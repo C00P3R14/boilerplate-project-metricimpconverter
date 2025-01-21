c7141a506c7d148340ba31e709bfc6c8ef3e861d
C00P3R14 <ngubane.linda9@gmail.com>
2025-01-21 16:02:44 +0200

    1 functional test works

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,18 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
+    this.timeout(5000);
+    suite("e2e Testing",() => {
+        test("Test GET /api/convert with valid input",(done) => {
+            chai.request(server)
+            .keepOpen()
+            .get("/api/convert?input=10L")
+            .end((req, res) => {
+                assert.equal(res.status,200);
+                assert.equal(res.text,'{"initNum":10,"initUnit":"l","returnNum":2.6417217685798895,"returnUnit":"gal","string":"10 liters converts to 2.6417217685798895 gallons"}')
+                done()
+            })
+        })
+    })
 
 });
